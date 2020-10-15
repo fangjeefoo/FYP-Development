03c6c0cdeb74cc3bcec0851279d60e8ce74ef559
foofangjee <51477859+fangjeefoo@users.noreply.github.com>
2020-10-16 01:51:42 +0800

    push V1 to git

---
+++ FYP Protoype V1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    public GameObject customer;
+
+    //private variable
+    private float counter = 0;
+    private int currentCustomer = 0;
+    private int maxCustomer = 5;
+
+    void Awake()
+    {
+        gm = this.GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if(counter > 2) //more than 2 seconds instantiate customer
+        {
+            if(currentCustomer < maxCustomer)
+            {
+                Instantiate(customer);
+                currentCustomer++;
+            }            
+            counter = 0f;
+        }
+    }
+}
