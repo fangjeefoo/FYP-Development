b2f03530619063a8ab1d86b0cc1afaa098431055
foofangjee <51477859+fangjeefoo@users.noreply.github.com>
2020-11-09 21:24:02 +0800

    Update
    + main menu clicking button done

--- FYP Protoype V1/Assets/Scripts/Manager/MainMenu.cs
+++ FYP Protoype V1/Assets/Scripts/Manager/MainMenu.cs
@@ -5,6 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private enum Choice { start, setting, quit };
+    private float counter;
+    private bool click;
+    private Choice choice;
+
+    public void Start()
+    {
+        counter = 0f;
+        click = false;
+    }
+
+    public void Update()
+    {
+        if (click)
+        {
+            counter += Time.deltaTime;                
+        }
+
+        if(counter >= 2.0f)
+        {
+            switch (choice)
+            {
+                case Choice.start:
+                    StartGame();
+                    break;
+                case Choice.setting:
+                    Setting();
+                    break;
+                case Choice.quit:
+                    QuitGame();
+                    break;
+            }
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
@@ -18,5 +53,29 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void StartGameOnEnter()
+    {
+        click = true;
+        choice = Choice.start;
+    }
+
+    public void SettingOnEnter()
+    {
+        click = true;
+        choice = Choice.setting;
+    }
+
+    public void QuitGameOnEnter()
+    {
+        click = true;
+        choice = Choice.quit;
+    }
+
+    public void OnExit()
+    {
+        click = false;
+        counter = 0f;
     }
 }
