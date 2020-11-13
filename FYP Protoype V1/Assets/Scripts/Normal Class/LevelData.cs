b61876c57750fe7aa0018d8d9a460025c4fc41b2
foofangjee <51477859+fangjeefoo@users.noreply.github.com>
2020-11-13 23:07:27 +0800

    Update
    + Performance Data class
    + LevelData Class

---
+++ FYP Protoype V1/Assets/Scripts/Normal Class/LevelData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData
+{
+    public int level;
+    public int[] exercisePerformedTimes;
+    public int score;
+
+    public LevelData(int level, int[] performedTimes, int score)
+    {
+        exercisePerformedTimes = new int[4];
+
+        this.level = level;
+        this.exercisePerformedTimes = performedTimes;
+        this.score = score;
+    }
+}
---
+++ FYP Protoype V1/Assets/Scripts/Normal Class/PerformanceData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PerformanceData
+{
+    public LevelData[] level;
+    public int totalDuration;
+    public bool[] selectedExercise;
+    public DateTime dateTime;
+
+    public PerformanceData(LevelData[] level, int totalDuration, bool[] selectedExercise)
+    {
+        this.selectedExercise = new bool[4];
+        this.level = level;
+        this.totalDuration = totalDuration;
+        this.selectedExercise = selectedExercise;
+
+        dateTime = DateTime.Now;
+    }
+}
