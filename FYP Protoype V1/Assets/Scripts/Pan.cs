7e66fd6da3d6e08952ee4cba55a5c2dd13936e98
foofangjee <51477859+fangjeefoo@users.noreply.github.com>
2020-11-23 13:32:50 +0800

    Update
    + scene done

---
+++ FYP Protoype V1/Assets/Scripts/Pan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision enter");
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision exit");
+    }
+}
